a222897d97635d6fc05fb94b05a2ced8db46a3b1
Jairo Lachira <45334322+j4iro@users.noreply.github.com>
2020-10-03 19:20:25 -0500

    se agrego vue router para home, about y error, esta quedando geniaaaaal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <main>
     <px-header />
-    <px-assets-table />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 
 <script>
 import PxHeader from "@/components/PxHeader";
-import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
-  components: { PxHeader, PxAssetsTable },
+  components: { PxHeader },
 };
 </script>
 
