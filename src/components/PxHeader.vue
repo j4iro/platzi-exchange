e79792e66cbee02d6ef75b8d8e39cab7f6085b82
Jairo Lachira <45334322+j4iro@users.noreply.github.com>
2020-10-17 17:12:05 -0500

    proyecto terminado

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -11,10 +11,19 @@
             Platzi Exchange
           </router-link>
         </div>
+
         <div
-          class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
+          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              v-for="l in links"
+              :key="l.title"
+              :to="l.to"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              >{{ l.title }}</router-link
+            >
+          </div>
         </div>
       </nav>
     </nav>
@@ -26,6 +35,12 @@ import PxIcon from "@/components/PxIcon"
 
 export default {
   name: "PxHeader",
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    }
+  },
   components: {
     PxIcon
   }
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -51,22 +51,27 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl"
+            >{{ convertResult }} {{ fromUsd ? `${asset.symbol}` : `USD` }}</span
+          >
         </div>
       </div>
 
@@ -122,11 +127,26 @@ export default {
       isLoading: false,
       asset: {},
       history: [],
-      markets: []
+      markets: [],
+      fromUsd: true,
+      convertValue: null
     }
   },
 
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0
+      }
+
+      const result =
+        this.fromUsd === true
+          ? this.convertValue / this.asset.priceUsd
+          : this.convertValue * this.asset.priceUsd
+
+      return result.toFixed(4)
+    },
+
     min() {
       return Math.min(
         ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
@@ -154,11 +174,20 @@ export default {
     }
   },
 
+  watch: {
+    $route() {
+      this.getCoin()
+    }
+  },
+
   created() {
     this.getCoin()
   },
 
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd
+    },
     getWebsite(exchange) {
       this.$set(exchange, "isLoading", true)
       return api
@@ -188,3 +217,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+td {
+  padding: 10px;
+  text-align: center;
+}
+</style>
